7a159af5a2d2cc3b9f5ce6982c809b40e938340a
superbento <37655893+superbento@users.noreply.github.com>
2019-11-23 22:09:16 +0100

    front end ver 1.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import MangaList from './MangaList'
+import AddManga from './AddManga'
+import EditManga from './EditManga'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Router>
+        <div className='col-md-6'>
+          <h1 className='text-center' style={style}>KinNekoJS</h1>
+          <Switch>
+            <Route path='/' exact component={MangaList} />
+            <Route path='/products' component={MangaList} />
+            <Route path='/add-product' component={AddManga} />
+            <Route path='/edit-product' component={EditManga} />
+
+          </Switch>
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+const style = {
+  color: 'black',
+  margin: '10px'
+}
+
+export default App
